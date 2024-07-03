d1295e38cf93a697a4da461cf51fb9d477f96dcc
Maciej Obuchowski <obuchowski.maciej@gmail.com>
2024-07-03 17:49:45 +0200

    openlineage: add config to include 'full' task info based on conf setting
    Signed-off-by: Maciej Obuchowski <obuchowski.maciej@gmail.com>

--- tests/providers/openlineage/test_conf.py
+++ tests/providers/openlineage/test_conf.py
@@ -28,6 +28,7 @@ from airflow.providers.openlineage.conf import (
     custom_extractors,
     dag_state_change_process_pool_size,
     disabled_operators,
+    include_full_task_info,
     is_disabled,
     is_source_enabled,
     namespace,
@@ -52,6 +53,7 @@ _CONFIG_OPTION_DISABLED = "disabled"
 _VAR_URL = "OPENLINEAGE_URL"
 _CONFIG_OPTION_SELECTIVE_ENABLE = "selective_enable"
 _CONFIG_OPTION_DAG_STATE_CHANGE_PROCESS_POOL_SIZE = "dag_state_change_process_pool_size"
+_CONFIG_OPTION_INCLUDE_FULL_TASK_INFO = "include_full_task_info"
 
 _BOOL_PARAMS = (
     ("1", True),
@@ -487,3 +489,20 @@ def test_dag_state_change_process_pool_size(var_string, expected):
     with conf_vars({(_CONFIG_SECTION, _CONFIG_OPTION_DAG_STATE_CHANGE_PROCESS_POOL_SIZE): var_string}):
         result = dag_state_change_process_pool_size()
         assert result == expected
+
+
+@pytest.mark.parametrize(
+    ("var", "expected"),
+    (
+        ("False", False),
+        ("True", True),
+        ("t", True),
+        ("a", False),
+        ("asdf", False),
+        ("31", False),
+        ("true", True),
+    ),
+)
+def test_include_full_task_info_reads_config(var, expected):
+    with conf_vars({(_CONFIG_SECTION, _CONFIG_OPTION_INCLUDE_FULL_TASK_INFO): var}):
+        assert include_full_task_info() is expected
